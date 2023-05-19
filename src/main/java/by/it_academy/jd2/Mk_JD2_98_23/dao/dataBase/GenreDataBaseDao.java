84971b85d9b0fc5659274e70edf792c17523f21c
Dzianis Tatur <tatdzi@tatdzi.local>
2023-05-19 11:22:04 +0300

    change database,dao

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/dataBase/GenreDataBaseDao.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/dataBase/GenreDataBaseDao.java
@@ -3,33 +3,79 @@ package by.it_academy.jd2.Mk_JD2_98_23.dao.dataBase;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.GenrDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.GenreCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IGenreDao;
-import by.it_academy.jd2.Mk_JD2_98_23.dao.factory.GenreDataBaseHandlerFactory;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.dataBase.connection.Const;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.dataBase.connection.DatabaseConnectin;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class GenreDataBaseDao implements IGenreDao {
-    GenreDatabaseHendler dbHendler;
+public class GenreDataBaseDao extends DatabaseConnectin implements IGenreDao {
+
 
 
     public GenreDataBaseDao() {
-        this.dbHendler = GenreDataBaseHandlerFactory.getInstance();
     }
 
 
 
     @Override
     public List<GenrDTO> get() {
-        return dbHendler.get();
+        List<GenrDTO> genreDTOList = new ArrayList<>();
+        String insert = "SELECT id, name FROM "+ Const.GENRE_TABLE;
+        try {
+            PreparedStatement ps = getDbConnection().prepareStatement(insert);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                genreDTOList.add(new GenrDTO(rs.getInt("id"),rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return genreDTOList;
     }
 
     @Override
     public GenrDTO get(int id) {
-        return dbHendler.get(id);
+        GenrDTO dto = null;
+        String insert = "SELECT id, name FROM "+Const.GENRE_TABLE+" WHERE id = "+id;
+        try {
+            PreparedStatement ps = getDbConnection().prepareStatement(insert);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                dto = new GenrDTO(rs.getInt(Const.GENRE_ID), rs.getString(Const.GENRE_NAME));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return dto;
     }
 
     @Override
     public synchronized GenrDTO save(GenreCreateDTO gen) {
-        dbHendler.save(gen.getName());
-        return dbHendler.get(gen.getName());
+        GenrDTO dto = null;
+        String insert = "INSERT INTO "+Const.GENRE_TABLE+"(name)  VALUES (?)";
+        try {
+            PreparedStatement ps = getDbConnection().prepareStatement(insert);
+            ps.setString(1,gen.getName());
+            ps.executeUpdate();
+            insert = "SELECT id, name FROM "+ Const.GENRE_TABLE+" WHERE name = "+gen.getName();
+            ps = getDbConnection().prepareStatement(insert);
+            ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    dto = new GenrDTO(rs.getInt(Const.GENRE_ID), rs.getString(Const.GENRE_NAME));
+                }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return dto;
     }
 }
--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/dataBase/connection/Configs.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/dataBase/connection/Configs.java
@@ -1,4 +1,4 @@
-package by.it_academy.jd2.Mk_JD2_98_23.dao.dataBase;
+package by.it_academy.jd2.Mk_JD2_98_23.dao.dataBase.connection;
 
 public class Configs {
     protected String dbHost = "localhost";
